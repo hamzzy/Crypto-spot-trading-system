9d02ab672d359e0f0f52cf8cb85c7935170f5492
ibrahim hamzat <hamat.ibrahim@gmail.com>
2021-11-20 23:13:34 +0100

    ª ERC20 complete with test

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,29 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Token", function () {
+  let Token;
+  let hardhatToken: any;
+  let owner: any;
+  let addr1: any;
+  let addr2: any;
+  let addrs: any;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
+    Token = await ethers.getContractFactory("Token");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    hardhatToken = await Token.deploy();
+  });
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  it("Should return the name of token", async function () {
+    expect(await hardhatToken.name()).to.equal("SquadTrade");
+    expect(await hardhatToken.symbol()).equal("SQT");
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  it("Testing for transfer", async function () {
+    await hardhatToken.transfer(addr1.address, 100);
+    const initialOwnerBalance = await hardhatToken.balanceOf(addr1.address);
+    expect(initialOwnerBalance).to.equal(100);
   });
 });
